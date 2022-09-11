81259c68db921d881b24b56accf28f9dc03e038d
James Kong <104340270+jameskongchief@users.noreply.github.com>
2022-09-12 01:24:55 +0800

    init

---
+++ RxSwiftMVVMEXample/ViewController.swift
@@ -0,0 +1,309 @@
+//
+//  ViewController.swift
+//  RxSwiftMVVMEXample
+//
+//  Created by James Kong on 11/9/2022.
+//
+
+import UIKit
+import RxSwift
+class ViewController: UIViewController {
+
+    var textField: UITextField = {
+       let view = UITextField()
+        view.placeholder = "Enter Text"
+        var bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: view.frame.height - 1, width: view.frame.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor.black.cgColor
+        view.borderStyle = .none
+        view.keyboardType = .decimalPad
+        view.backgroundColor = .darkGray
+        view.layer.addSublayer(bottomLine)
+        view.textColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var switch1: UISwitch = {
+       let view = UISwitch()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var switch2: UISwitch = {
+       let view = UISwitch()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var switch3: UISwitch = {
+       let view = UISwitch()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var button: UIButton = {
+       let view = UIButton()
+        view.setTitleColor(.blue, for: .normal)
+        view.setTitleColor(.lightGray, for: .disabled)
+        view.setTitle("SHOW BOTTOM SHEET", for: .normal)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    /// Dimension and Size
+    private enum Dimen {
+        /* Example
+         static let headerViewTopPadding = IPhoneXRelative320LayoutConstraint.value(18)
+         */
+    }
+
+    private(set) var viewModel: MainViewModelType!
+
+
+    /// Combine related:  handle lifecycle of binding/subscription
+    private var bag = DisposeBag()
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        viewModel = MainViewModel()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(nibName: nil, bundle: nil)
+        viewModel = MainViewModel()
+    }
+    /**
+     - Remark: It should call only once and before ViewController calling viewDidLoad(), In ViewDidLoad, it call bindViewModel().
+     */
+    public func setupViewModel(_ viewModel: MainViewModelType) {
+        self.viewModel = viewModel
+    }
+
+    /// Light or Dark Mode handling
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        setupViewsAndConstraints()
+        setupLayout()
+        setupLocalization()
+        bindViewModel()
+        viewModel.inputs.viewDidLoad()
+    }
+
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.inputs.viewWillAppear()
+    }
+
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.inputs.viewDidAppear()
+    }
+
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        viewModel.inputs.viewDidDisappear()
+    }
+
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewModel.inputs.viewWillDisappear()
+    }
+
+    private func setupNavigationBar() {
+
+    }
+
+    private func setupViewsAndConstraints() {
+        self.view.backgroundColor = .white
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(textField)
+        stackView.addArrangedSubview(switch1)
+        stackView.addArrangedSubview(switch2)
+        stackView.addArrangedSubview(switch3)
+        stackView.addArrangedSubview(button)
+
+        scrollView.addSubview(stackView)
+        view.addSubview(scrollView)
+
+
+
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32),
+            stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    private func setupLayout() {
+        // TODO: setup colors, textColors, backgroundColors, images
+    }
+
+    private func setupLocalization() {
+        // TODO: get localized string from CHFLanguage.getText(key:"__KEY__", file: CHIEF_UI_STRINGS)
+    }
+
+    private func bindViewModel() {
+
+
+        button.rx.tap.subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
+            self.viewModel.inputs.onButtonClick()
+        }).disposed(by: bag)
+
+        self.viewModel.inputs.setText(text: textField.text ?? "")
+
+        textField
+            .rx
+            .text
+            .asDriver()
+            .asObservable()
+            .subscribe(onNext: { [weak self] text in
+                guard let self = self else { return }
+                self.viewModel.inputs.setText(text: text ?? "")
+            }).disposed(by: bag)
+
+        switch1.rx.isOn
+            .subscribe(onNext: { [weak self] isOn in
+            guard let self = self else { return }
+            print("\(isOn)")
+            self.viewModel.inputs.enable1(value: isOn)
+        }).disposed(by: bag)
+
+        switch2.rx.isOn
+            .subscribe(onNext: { [weak self] isOn in
+            guard let self = self else { return }
+            print("\(isOn)")
+            self.viewModel.inputs.enable2(value: isOn)
+        }).disposed(by: bag)
+
+        switch3.rx.isOn
+            .subscribe(onNext: { [weak self] isOn in
+            guard let self = self else { return }
+            print("\(isOn)")
+            self.viewModel.inputs.enable3(value: isOn)
+        }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch1Enabled
+            .asObservable()
+            .subscribe(onNext: { [weak self] enabled in
+                guard let self = self else { return }
+                self.switch1.isEnabled = enabled
+
+                if enabled == false {
+                    self.switch2.isEnabled = enabled
+                    self.switch3.isEnabled = enabled
+                    self.switch1.setOn(false, animated: true)
+                    self.switch2.setOn(false, animated: true)
+                    self.switch3.setOn(false, animated: true)
+                }
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch2Enabled
+            .asObservable()
+            .subscribe(onNext: { [weak self] enabled in
+                guard let self = self else { return }
+                self.switch2.isEnabled = enabled
+
+                if enabled == false {
+                    self.switch3.isEnabled = enabled
+                    self.switch2.setOn(false, animated: true)
+                    self.switch3.setOn(false, animated: true)
+                }
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch3Enabled
+            .asObservable()
+            .subscribe(onNext: { [weak self] enabled in
+                guard let self = self else { return }
+                self.switch3.isEnabled = enabled
+                if enabled == false {
+                    self.switch3.setOn(false, animated: true)
+                }
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch1Value
+            .asObservable()
+            .subscribe(onNext: { [weak self] value in
+                guard let self = self else { return }
+                self.switch1.isOn = value
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch2Value
+            .asObservable()
+            .subscribe(onNext: { [weak self] value in
+                guard let self = self else { return }
+                self.switch2.isOn = value
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .switch3Value
+            .asObservable()
+            .subscribe(onNext: { [weak self] value in
+                guard let self = self else { return }
+                self.switch3.isOn = value
+            }).disposed(by: bag)
+
+
+        viewModel
+            .outputs
+            .enable1
+            .asObservable()
+            .subscribe(onNext: { [weak self] value in
+                guard let self = self else { return }
+                print("enableButton \(value)")
+                self.switch1.isOn = value
+            }).disposed(by: bag)
+
+        viewModel
+            .outputs
+            .enableButton
+            .asObservable()
+            .subscribe(onNext: { [weak self] enabled in
+                guard let self = self else { return }
+                print("enableButton \(enabled)")
+                self.button.isEnabled = enabled
+            }).disposed(by: bag)
+
+
+        viewModel
+            .outputs
+            .didClickButton
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+//                self.presentPanModal(MySelectionBottomSheet(viewModel: self.viewModel))
+            }).disposed(by: bag)
+
+
+
+    }
+
+}
+
---
+++ RxSwiftMVVMEXample/ViewModel/MainViewModel.swift
@@ -0,0 +1,241 @@
+//
+//  MainViewModel.swift
+//  RxSwiftMVVMEXample
+//
+//  Created by James Kong on 11/9/2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public func ignoreNil<A>(x: A?) -> Driver<A> {
+    x.map { Driver.just($0) } ?? Driver.empty()
+}
+
+#if os(iOS) || os(tvOS)
+
+import RxSwift
+import UIKit
+#endif
+public protocol MainViewModelInputs: AnyObject {
+    func viewDidLoad()
+    func viewDidAppear()
+    func viewDidDisappear()
+    func viewWillAppear()
+    func viewWillDisappear()
+
+    func setText(text: String)
+    func setSelectedOption(option: String)
+    func enable1(value: Bool)
+    func enable2(value: Bool)
+    func enable3(value: Bool)
+    func getOptions2()
+    func onButtonClick()
+}
+
+public protocol MainViewModelOutputs: AnyObject {
+    var switch1Enabled: Driver<Bool> { get }
+    var switch2Enabled: Driver<Bool> { get }
+    var switch3Enabled: Driver<Bool> { get }
+
+    var switch1Value: Driver<Bool> { get }
+    var switch2Value: Driver<Bool> { get }
+    var switch3Value: Driver<Bool> { get }
+    var enableButton: Driver<Bool> { get}
+    var text: Driver<String> { get}
+    var options1: Driver<[String]> { get}
+    var options2: Driver<[String]> { get}
+    var selectedOption: Driver<String> { get}
+    var didClickButton: Driver<Void> { get}
+    var progressBarVisibility: Driver<Int> { get}
+    var showBottomSheet: Driver<String> { get}
+    var enable1: Driver<Bool> { get}
+    var enable2: Driver<Bool> { get}
+    var enable3: Driver<Bool> { get}
+    var bottomsheetOptions: Driver<([String], String?)> { get}
+}
+
+public protocol MainViewModelType: AnyObject {
+    var inputs: MainViewModelInputs { get }
+    var outputs: MainViewModelOutputs { get }
+}
+
+class MainViewModel:
+    NSObject,
+    MainViewModelType,
+    MainViewModelInputs,
+    MainViewModelOutputs {
+
+
+    func setText(text: String) {
+        _text.accept(text)
+    }
+
+    func setSelectedOption(option: String) {
+        _selectedOption.accept(option)
+    }
+
+    func enable1(value: Bool) {
+        _enable1.accept(value)
+    }
+
+    func enable2(value: Bool) {
+        _enable2.accept(value)
+    }
+
+    func enable3(value: Bool) {
+        _enable3.accept(value)
+    }
+
+    func getOptions2() {
+        _options2.accept([
+        ]
+        )
+    }
+
+    func onButtonClick() {
+        _didClickButton.accept(())
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            guard let self = self else { return }
+            self.getOptions2()
+        }
+
+    }
+
+    var switch1Enabled: Driver<Bool> {
+        _text.map { $0?.isEmpty == false }
+            .asObservable()
+            .asDriver(onErrorJustReturn: false)
+            .flatMap(ignoreNil)
+
+    }
+
+
+    var switch2Enabled: Driver<Bool> {
+        switch1Value
+            .flatMap(ignoreNil)
+            .asDriver()
+    }
+
+    var switch3Enabled: Driver<Bool> {
+        switch2Value
+            .flatMap(ignoreNil)
+            .asDriver()
+    }
+
+    var switch1Value: Driver<Bool> {
+        _enable1
+            .withLatestFrom(
+                _text,
+                resultSelector: {
+                    $0 == true && $1?.isEmpty == false
+                })
+            .asDriver(onErrorJustReturn: false)
+            .flatMap(ignoreNil)
+            .distinctUntilChanged()
+
+    }
+
+    var switch2Value: Driver<Bool> {
+        _enable2
+            .withLatestFrom(
+                switch1Value,
+                resultSelector: {
+                    $0 == true && $1 == true
+                })
+            .asDriver(onErrorJustReturn: false)
+            .flatMap(ignoreNil)
+    }
+
+    var switch3Value: Driver<Bool> {
+        _enable3
+            .withLatestFrom(
+                switch2Value
+                , resultSelector: {
+
+                    $0 == true && $1 == true
+                })
+            .asDriver(onErrorJustReturn: false)
+            .flatMap(ignoreNil)
+    }
+
+    private var _isAllEnabled: Observable<Bool> {
+        BehaviorRelay
+            .combineLatest(
+                _enable1.compactMap{ $0 },
+                _enable2.compactMap{ $0 },
+                _enable3.compactMap{ $0 }
+            ) { a, b, c -> Bool in
+                a == true && b == true && c == true
+            }
+    }
+
+    var enableButton: Driver<Bool> {
+        _text.compactMap{ $0 }
+            .withLatestFrom(_isAllEnabled) { text, isAllEnabled in
+                return (text != "" && isAllEnabled)
+            }
+            .asObservable()
+            .flatMap(ignoreNil)
+            .asDriver(onErrorJustReturn: false)
+    }
+
+    var text: Driver<String> { _text.asDriver().flatMap(ignoreNil) }
+    private var _text: BehaviorRelay<String?> = .init(value: nil)
+
+    var options1: Driver<[String]> { _options1.asDriver().flatMap(ignoreNil) }
+    private var _options1: BehaviorRelay<[String]?> = .init(value: nil)
+
+    var selectedOption: Driver<String> { _selectedOption.asDriver().flatMap(ignoreNil) }
+    private var _selectedOption: BehaviorRelay<String?> = .init(value: nil)
+
+    var options2: Driver<[String]> { _options2.asDriver().flatMap(ignoreNil) }
+    private var _options2: BehaviorRelay<[String]?> = .init(value: nil)
+
+    var didClickButton: Driver<Void> { _didClickButton.asDriver().flatMap(ignoreNil) }
+    private var _didClickButton: BehaviorRelay<Void?> = .init(value: nil)
+
+    var progressBarVisibility: Driver<Int> { _progressBarVisibility.asDriver().flatMap(ignoreNil) }
+    private var _progressBarVisibility: BehaviorRelay<Int?> = .init(value: nil)
+
+    var showBottomSheet: Driver<String> { _showBottomSheet.asDriver().flatMap(ignoreNil) }
+    private var _showBottomSheet: BehaviorRelay<String?> = .init(value: nil)
+
+    var enable1: Driver<Bool> { _enable1.asDriver().flatMap(ignoreNil) }
+    private var _enable1: BehaviorRelay<Bool?> = .init(value: nil)
+
+    var enable2: Driver<Bool> { _enable2.asDriver().flatMap(ignoreNil) }
+    private var _enable2: BehaviorRelay<Bool?> = .init(value: nil)
+
+    var enable3: Driver<Bool> { _enable3.asDriver().flatMap(ignoreNil) }
+    private var _enable3: BehaviorRelay<Bool?> = .init(value: nil)
+
+    var bottomsheetOptions: Driver<([String], String?)> { _bottomsheetOptions.asDriver().flatMap(ignoreNil) }
+    private var _bottomsheetOptions: BehaviorRelay<([String], String?)?> = .init(value: nil)
+
+
+    var inputs: MainViewModelInputs { self }
+    var outputs: MainViewModelOutputs { self }
+
+    /* initialize Some Usecase or AppState */
+
+    override init(/*Any dependency*/) {
+
+    }
+
+    func viewDidLoad() { }
+
+    func viewDidAppear() { }
+
+    func viewDidDisappear() { }
+
+    func viewWillAppear() { }
+
+    func viewWillDisappear() { }
+
+    // extra Business logic
+    var cursorPosition: Int? = 0
+}
+
+
